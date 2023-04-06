78d333d3dc81013e84fa1bf60745ed97d3fae29f
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-04-06 16:59:12 -0300

    update homepage component

--- app/modules/homepage/index.jsx
+++ app/modules/homepage/index.jsx
@@ -4,11 +4,47 @@ export const links = () => {
   return [{rel: 'stylesheet', href: styles}];
 };
 
-const Homepage = () => {
+/**
+ * @class
+ * @param {object} props 
+ * @param {string} props.prop1 - Property 1.
+ * @param {number} props.prop2 - Property 2.
+ */
+const Homepage = ({prop1, prop2}) => {
+
+  /**
+   * page's copy
+   * @member
+   * @type {string}
+   */
+  const COPY = 'Here is the homepage';
+
+  /**
+   * Retrieves the page's title 
+   * @method
+   * @author Wilson Freitas 
+   * @param {string} title - page's title
+   * @param {number} id - ID to be concatenated 
+   * @returns page's title concatenated with the ID
+   */
+  function getTitle(title, id){
+    return `${title} - #${id}`;
+  }
+
+  /**
+   * Retrieves the page's copy 
+   * @method
+   * @author Wilson Freitas
+   * @returns page's copy
+   */
+  function getCopy(){
+    return COPY;
+  }
+
   return (
     <section className="mainContainer">
-      <h3 className="title">Hello!</h3>
-      <p className="content">Here is the homepage</p>
+      <h3 className="title">{getTitle('Welcome to our site', 2)}</h3>
+      <p className="content">{getCopy()}</p>
     </section>
   );
 };
