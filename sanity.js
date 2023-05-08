62aaafcae1eb24ac1f14826be1908e9827704cd8
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-05-08 16:28:45 -0300

    remove old sanity.js

--- sanity.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-import { definePreview } from '@sanity/preview-kit';
-
-export const projectId = 'mstl3bgb';
-export const dataset = 'production';
-export const apiVersion = '2023-03-20';
-
-export const client = createClient({ projectId, dataset, apiVersion, useCdn: false });
-export const usePreview = definePreview({ projectId, dataset });
