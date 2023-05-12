452fddbadc34761a8e34fa7eef04cb71dc5313e5
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-05-11 22:25:28 -0300

    update pets name

--- app/routes/($lang)/index.jsx
+++ app/routes/($lang)/index.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from '@remix-run/react';
 import apolloClient from '~/utils/graphql/sanity/apolloClient';
-import { GET_ALL_PETS } from '~/utils/graphql/sanity/queries/pet';
+import { GET_ALL_PETS } from '~/utils/graphql/sanity/queries/pets';
 import Layouts, { links as layoutsStyles } from '~/layouts';
 import Homepage, { links as homePageStyles } from '~/modules/homepage';
 
@@ -16,19 +16,19 @@ export const loader = async ({context}) => {
   const { SANITY_DATASET_DOMAIN, SANITY_API_TOKEN } = context.env;
   
   const result = await apolloClient(SANITY_DATASET_DOMAIN, SANITY_API_TOKEN).query({ query: GET_ALL_PETS });
-  const { allPet } = result.data;
+  const { allPets } = result.data;
 
-  return { allPet };
+  return { allPets };
 
 };
 
 export default function Index() {
 
-  const { allPet } = useLoaderData();
+  const { allPets } = useLoaderData();
 
   return (
     <Layouts.MainNavFooter>
-      <Homepage pets={allPet} />
+      <Homepage pets={allPets} />
     </Layouts.MainNavFooter>
   );
 }
