115f94286f4556f0d65d0fafb85a6c779a6d702f
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-05-23 20:46:48 -0300

    migrate footerLogo

---
+++ app/modules/footerLogo/index.jsx
@@ -0,0 +1,26 @@
+import styles from './styles.css';
+
+export const links = () => {
+  return [
+    { rel: 'stylesheet', href: styles },
+  ];
+};
+
+const mockLogo = {
+  image: 'https://cdn.shopify.com/s/files/1/1736/9637/t/77/assets/tula-skincare-logo.png?v=5746727401723605709',
+  linkUrl: '/'
+};
+
+const FooterLogo = ({ logo, ...rest }) => {
+  logo = logo || mockLogo;
+
+  return (
+    <div {...rest} id={'footerLogo'} >
+      <a alt="Tula Logo" href={logo.linkUrl}>
+        <img src={logo.image} className={'footerImg'} />
+      </a>
+    </div>
+  );
+};
+
+export default FooterLogo;
