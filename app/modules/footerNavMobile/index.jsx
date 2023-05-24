f74719ba6ad50a71d9257765722484273e7b0ec7
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-05-23 21:00:32 -0300

    migrate footerNavMobile

---
+++ app/modules/footerNavMobile/index.jsx
@@ -0,0 +1,74 @@
+import classnames from 'classnames';
+import { Link } from '@remix-run/react';
+
+import styles from './styles.css';
+
+export const links = () => {
+  return [
+    { rel: 'stylesheet', href: styles },
+  ];
+};
+
+const classes = {
+  footerMobileNav: classnames('footer__mobileNav'),
+  mobileNavListItem: classnames('mobileNav_listItem'),
+  footerMobileSubNav: classnames('footer__mobileSubNav'),
+  mobileSubNavListItem: classnames('mobileSubNav_listItem'),
+  listItemLinkContainer: classnames('linkItem_link__container'),
+  listItemLink: classnames('listItem_link'),
+  listItemLinkBold: classnames('listItem_link', 'fw_bold')
+};
+
+const FooterSubNavMobile = ({ navLinks = [] }) => (
+  <ul className={classes.footerMobileSubNav}>
+    {
+      navLinks.map(linkItem =>
+        <li key={linkItem._id} className={classes.mobileSubNavListItem}>
+          <Link to={linkItem.url} className={classes.listItemLink}>
+            {linkItem.displayText}
+          </Link>
+        </li>
+      )
+    }
+  </ul>
+);
+
+const FooterNavMobile = ({ navLinkGroups = [] }) => {
+
+  const handleDropdown = (e) => {
+    if (e.currentTarget.classList.contains(styles.active)) {
+      e.currentTarget.classList.remove(styles.active);
+      e.currentTarget.children[0].children[1].textContent = '+';
+    } else {
+      e.currentTarget.classList.add(styles.active);
+      e.currentTarget.children[0].children[1].textContent = 'U+2013';
+    }
+  };
+
+  return (
+    <ul className={classes.footerMobileNav}>
+      {
+        navLinkGroups.map(linkItem => linkItem.navLinks && linkItem.navLinks.length > 0 ? (
+          <li key={linkItem._id} className={classes.mobileNav_listItem} onClick={(e) => handleDropdown(e)}>
+            <div className={classes.listItemLinkContainer}>
+              <span to={linkItem.url} className={classes.listItemLinkBold}>{linkItem.displayText}</span>
+              <span className={classes.listItemLinkBold} >+</span>
+            </div>
+            <FooterSubNavMobile navLinks={linkItem.navLinks} />
+          </li>
+        ) : (
+          <li key={linkItem._id} className={classes.mobileNav_listItem}>
+            <div className={classes.listItemLinkContainer}>
+              <Link to={linkItem.url} className={classes.listItemLinkBold} style={linkItem?.fontColorHex ? { color: linkItem?.fontColorHex } : undefined}>
+                {linkItem.displayText}
+                {linkItem?.emoji && <img src={linkItem.emoji?.src} />}
+              </Link>
+            </div>
+          </li>
+        )
+        )
+      }
+    </ul>
+  );
+};
+export default FooterNavMobile;
