279d139cd4e0d27b3607769a7e8ab8f214f0e812
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-05-26 20:06:24 -0300

    migrate listrak comp

---
+++ app/modules/listrack/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { appendScript } from '~/utils/functions/getApiKeys';
+import getApiKeys from '~/utils/functions/getApiKeys';
+
+const Listrack = ({ productList }) => {
+
+  const _ltk = _ltk;
+
+  React.useEffect(() => {
+
+    const triggerListrak = () => {
+
+      _ltk.Signup.New('Footer', 'email_signup_new', _ltk.Signup.TYPE.CLICK, 'FooterNewsletterSubmit', 'channel');
+      _ltk.OnsiteContent?.reload();
+
+      if (window.location.href.includes('/products/')) {
+
+        const slug = window.location.pathname.replace('/products/', '');
+
+        const product = productList.products.filter((prod) => (prod.slug === slug));
+
+        _ltk.Activity.AddProductBrowse(product[0].externalId.toString());
+
+      }
+
+      _ltk.Activity.AddPageBrowse(window.location.href);
+
+    };
+
+    if (typeof _ltk === 'undefined') {
+      appendScript(getApiKeys().LISTRAK_SCRIPT, 'listrak', true, triggerListrak);
+    } else {
+      triggerListrak();
+    }
+
+  }, []);
+
+  return null;
+
+};
+
+export default Listrack;
