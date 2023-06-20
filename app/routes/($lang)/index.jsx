f7a8e7d1f615b50af34d602e2ac8a5a426abb1cf
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-06-20 20:16:57 -0300

    refactor index.js loader

--- app/routes/($lang)/index.jsx
+++ app/routes/($lang)/index.jsx
@@ -1,9 +1,8 @@
 import { useLoaderData } from '@remix-run/react';
-import { getGroupOfCMSContent } from '~/utils/functions/eventFunctions';
-import { getCollectionProducts } from '~/utils/graphql/shopify/queries/collections';
 import Layouts, { links as layoutsStyles } from '~/layouts';
 import Homepage, { links as homePageStyles } from '~/modules/homepage';
-
+import { getGroupOfCMSContent } from '~/utils/functions/eventFunctions';
+import { getCollectionProducts } from '~/utils/graphql/shopify/queries/collections';
 import { GET_FOOTERS, GET_EMAIL_SMS_SIGNUP_CONTENT, GET_CART_PAGE_CONFIG, GET_ANNOUNCEMENT_HEADER, GET_ANNOUNCEMENT_MESSAGES, GET_MOBILE_NAV_BAR, GET_HEADER_CONFIG, GET_MOBILE_NAV_FOOTER_MAIN_BUTTON, GET_ANNOUNCEMENT_TOP_BANNER, GET_SITE_WIDE_SETTINGS, GET_SEARCH_CONFIG } from '~/utils/graphql/sanity/queries';
 
 export const links = () => {
