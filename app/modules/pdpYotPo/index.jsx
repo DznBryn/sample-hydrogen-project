8cf9526bd5bd4ece7ffb6dc2e978ccfe1f03659f
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-08-09 16:02:07 -0300

    migrate PDPYotPo

---
+++ app/modules/pdpYotPo/index.jsx
@@ -0,0 +1,21 @@
+import PDPYotPoReviews from '../pdpYotPoReviews';
+import styles from './styles.css';
+
+export const links = () => {
+  return [
+    { rel: 'stylesheet', href: styles },
+  ];
+};
+
+const PDPYotPo = ({ product, YotpoRef }) => {
+  let curProduct;
+  curProduct = product;
+
+  return (
+    <div ref={YotpoRef} className={'yotpoWrapper'}>
+      <PDPYotPoReviews {...curProduct} />
+    </div>
+  );
+};
+
+export default PDPYotPo;
