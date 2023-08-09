38ff6c71d43c523356b14a49513b28afafd24118
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-08-09 16:08:35 -0300

    migrate fireWorkPDPCarousel

---
+++ app/modules/fireWorkPDPCarousel/index.jsx
@@ -0,0 +1,26 @@
+import FireWorkCarousel, { links as fireWorkCarouselStyle } from '../fireWorkCarousel';
+import { PDPSliderPanelTitle } from '../pdpPanelSlider';
+
+import styles from './styles.css';
+
+export const links = () => {
+  return [
+    { rel: 'stylesheet', href: styles },
+    ...fireWorkCarouselStyle(),
+  ];
+};
+
+const FireWorkPDPCarousel = ({ playlist }) => {
+  return (
+    <div>
+      <div className={'fireWorkPDPCarouselHeader'}>
+        <PDPSliderPanelTitle data={{ title: 'Hear from our glow-getters' }} />
+      </div>
+      <div className={'fireWorkPDPCarouselWrapper'}>
+        <FireWorkCarousel playlist={playlist} />
+      </div>
+    </div>
+  );
+};
+
+export default FireWorkPDPCarousel;
