70bb8634146e2b7e727bafe5aa6f9963434874b1
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-09-12 12:32:42 -0300

    fix footerCopyright

--- app/modules/footer/footerCopyright/index.jsx
+++ app/modules/footer/footerCopyright/index.jsx
@@ -16,9 +16,9 @@ const FooterCopyright = () => {
       `| <div id="tula-ot-button-container"><button id="ot-sdk-btn" class="ot-sdk-show-settings"> ${getApiKeys().ONE_TRUST.otButtonMessage}</button></div>`
     ) : '';
 
-    let footerCopyrightOT = `© TULA Life, Inc. | All Rights Reserved | Made in NYC | <a href="/pages/terms-conditions" target="_blank">Terms &amp; Conditions</a> | <a href="/pages/privacy-policy" target="_blank">Privacy Policy</a> | <a href="/pages/cookie-policy" target="_blank">Cookie Policy</a> ${otButtonMessage}`;
+    let footerCopyrightOT = `© TULA Life, Inc. | All Rights Reserved | Made in NYC | <a href="/pages/terms-conditions" target="_target">Terms &amp; Conditions</a> | <a href="/pages/privacy-policy" target="_target">Privacy Policy</a> | <a href="/pages/cookie-policy" target="_target">Cookie Policy</a> ${otButtonMessage}`;
 
-    let footerCopyright = '© TULA Life, Inc. | All Rights Reserved | Made in NYC | <a href="/pages/terms-conditions" target="_blank">Terms &amp; Conditions</a> | <a href="/pages/privacy-policy" target="_blank">Privacy Policy</a> | <a href="/pages/cookie-policy" target="_blank">Cookie Policy</a>';
+    let footerCopyright = '© TULA Life, Inc. | All Rights Reserved | Made in NYC | <a href="/pages/terms-conditions" target="_target">Terms &amp; Conditions</a> | <a href="/pages/privacy-policy" target="_target">Privacy Policy</a> | <a href="/pages/cookie-policy" target="_target">Cookie Policy</a>';
 
     getApiKeys().ONE_TRUST
       ? (document.getElementById('footerCopyright').innerHTML = footerCopyrightOT)
