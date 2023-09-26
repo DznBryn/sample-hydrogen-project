aaafc6b99da4a860555049f08c62f10cd23ede03
BrianDemorcy <501526@containerstore.com>
2023-09-26 11:52:41 -0400

    Update UpdateItems api and component

--- app/modules/sliderCart/sliderCartProductBox/index.jsx
+++ app/modules/sliderCart/sliderCartProductBox/index.jsx
@@ -195,15 +195,27 @@ const RegularProduct = ({ item, isSellingPlan = false, sellingPlanName, handleSu
       </div>
       <div className={'productQty'}>
         <div className={'product_input'}>
-          <button type="button" className={'minus'} onClick={handleSub}>
-            <span></span><span style={{ display: 'none' }} className="ae-compliance-indent"> Reduce Quantity </span>
-            <span style={{ display: 'none' }} className="ae-compliance-indent"> {item?.title}  </span>
-          </button>
+          <UpdateItemButton lineIds={[
+            {
+              id: item?.line_item_id,
+              quantity: item?.quantity && item.quantity > 1 ? Number(Math.max(0, item.quantity - 1).toFixed(0)) : 1
+            }]}>
+            <button type="submit" className={'minus'}>
+              <span></span><span style={{ display: 'none' }} className="ae-compliance-indent"> Reduce Quantity </span>
+              <span style={{ display: 'none' }} className="ae-compliance-indent"> {item?.title}  </span>
+            </button>
+          </UpdateItemButton>
           <input type="text" value={item?.quantity} min="0" ref={inputQtyRef} readOnly="readonly" name="updates[33187716673:307c12b24eaffc6df04a594677e63385]" className="product-quantity" id={`quantity--${item.id}`} aria-label="Quantity" />
-          <button type="button" className={'plus'} onClick={handleAdd}>
-            <span></span><span style={{ display: 'none' }} className="ae-compliance-indent"> Increase Quantity </span>
-            <span style={{ display: 'none' }} className="ae-compliance-indent"> {item?.title}  </span>
-          </button>
+          <UpdateItemButton lineIds={[
+            {
+              id: item?.line_item_id,
+              quantity: Number((item?.quantity + 1).toFixed(0))
+            }]}>
+            <button type="submit" className={'plus'} >
+              <span></span><span style={{ display: 'none' }} className="ae-compliance-indent"> Increase Quantity </span>
+              <span style={{ display: 'none' }} className="ae-compliance-indent"> {item?.title}  </span>
+            </button>
+          </UpdateItemButton>
           <div className={'productTotal'}>
             <h6>{getCurrency() + Number(item?.cost?.totalAmount?.amount).toFixed(2)}</h6>
             {/* {forceChange && (
@@ -304,6 +316,24 @@ function RemoveItemButton({ lineId }) {
         <path d="M0.641509 4.16981H1.8055L2.72767 18.0049C2.75022 18.342 3.02963 18.6038 3.36792 18.6038H13.6321C13.9704 18.6038 14.2498 18.342 14.2723 18.0049L15.1945 4.16981H16.3585C16.7131 4.16981 17 3.88289 17 3.5283C17 3.17372 16.7131 2.88679 16.3585 2.88679H12.0283V0.64151C12.0283 0.286915 11.7414 0 11.3868 0H5.61321C5.25861 0 4.9717 0.286925 4.9717 0.64151V2.88679H0.641509C0.286915 2.88679 0 3.17372 0 3.5283C0 3.88289 0.286925 4.16981 0.641509 4.16981ZM11.5196 13.0182C11.7702 13.2687 11.7702 13.6747 11.5196 13.9253C11.3943 14.0506 11.2302 14.1132 11.066 14.1132C10.9019 14.1132 10.7377 14.0506 10.6124 13.9253L8.49997 11.8128L6.38751 13.9253C6.26222 14.0506 6.09808 14.1132 5.93393 14.1132C5.76978 14.1132 5.60567 14.0506 5.48035 13.9253C5.22976 13.6747 5.22976 13.2687 5.48035 13.0182L7.59281 10.9057L5.48035 8.79323C5.22976 8.54265 5.22976 8.13668 5.48035 7.88611C5.73094 7.63554 6.1369 7.63552 6.38748 7.88611L8.49994 9.99857L10.6124 7.88611C10.863 7.63552 11.2689 7.63552 11.5195 7.88611C11.7701 8.1367 11.7701 8.54266 11.5195 8.79323L9.40706 10.9057L11.5196 13.0182ZM6.25472 1.28302H10.7453V2.88679H6.25472V1.28302Z" fill="black" fillOpacity="0.3" />
       </svg>
     </button>
+  </fetcher.Form>;
+}
+
+function UpdateItemButton({ lineIds, children }) {
+  const fetcher = useFetcher();
+  const { updateCart: updateItemData = () => { }, data = null } = useStore(store => store?.cart ?? {});
+  const lines = JSON.stringify(lineIds);
+  useEffect(() => {
+    if (fetcher.type === FETCHER.TYPE.ACTION_RELOAD) {
+      if (fetcher?.data?.cart?.totalQuantity && fetcher?.data?.cart?.totalQuantity !== data?.totalQuantity) {
+        updateItemData(fetcher.data.cart);
+      }
+    }
+  }, [fetcher.type]);
+  return <fetcher.Form action="/cart" method={API_METHODS.POST}>
+    <input type="hidden" name="cartAction" value={'UPDATE_CART'} />
+    <input type="hidden" name="lines" value={lines} />
+    {children}
   </fetcher.Form>;
 }
 export default SliderCartProductBox;
