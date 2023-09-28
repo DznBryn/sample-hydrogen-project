28cf6e68bb8895c3df896ee2a875500b29fed390
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-09-28 04:06:31 -0300

    :sparkles: feat: Change MainNav position to Sticky

--- app/modules/mainNav/index.jsx
+++ app/modules/mainNav/index.jsx
@@ -1,21 +1,41 @@
-import { useRef, useEffect } from 'react';
-import { createCustomEvent } from '~/utils/functions/eventFunctions';
-import DesktopNavItem, { links as desktopNavItemStyles } from '~/modules/mainNav/desktopNavItem';
-import HeaderIcons, { links as headerIconsStyles } from '~/modules/mainNav/headerIcons';
-import HeaderLogo, { links as headerLogoStyles } from '~/modules/mainNav/headerLogo';
-import NavTopHeader, { links as navTopHeaderStyles } from '~/modules/mainNav/navTopHeader';
-import MobileNavbar, { links as mobileNavbarStyles } from '~/modules/mainNav/mobileNavbar';
-import MainNavMobileOverlay, { links as mainNavMobileOverlayStyles } from '~/modules/mainNav/mainNavMobileOverlay';
-import PromoOfferBar, { links as promoOfferBarStyles } from '~/modules/mainNav/promoOfferBar';
-import IconSearch, { links as iconSearchStyles } from '~/modules/mainNav/iconSearch';
-import CountdownTimerBar, { links as countdownTimerBarStyles } from '~/modules/mainNav/countdownTimerBar';
-import AnnouncementTopBanner, { links as announcementTopBannerStyles } from '~/modules/mainNav/announcementTopBanner';
+import {useRef, useEffect} from 'react';
+import {createCustomEvent} from '~/utils/functions/eventFunctions';
+import DesktopNavItem, {
+  links as desktopNavItemStyles,
+} from '~/modules/mainNav/desktopNavItem';
+import HeaderIcons, {
+  links as headerIconsStyles,
+} from '~/modules/mainNav/headerIcons';
+import HeaderLogo, {
+  links as headerLogoStyles,
+} from '~/modules/mainNav/headerLogo';
+import NavTopHeader, {
+  links as navTopHeaderStyles,
+} from '~/modules/mainNav/navTopHeader';
+import MobileNavbar, {
+  links as mobileNavbarStyles,
+} from '~/modules/mainNav/mobileNavbar';
+import MainNavMobileOverlay, {
+  links as mainNavMobileOverlayStyles,
+} from '~/modules/mainNav/mainNavMobileOverlay';
+import PromoOfferBar, {
+  links as promoOfferBarStyles,
+} from '~/modules/mainNav/promoOfferBar';
+import IconSearch, {
+  links as iconSearchStyles,
+} from '~/modules/mainNav/iconSearch';
+import CountdownTimerBar, {
+  links as countdownTimerBarStyles,
+} from '~/modules/mainNav/countdownTimerBar';
+import AnnouncementTopBanner, {
+  links as announcementTopBannerStyles,
+} from '~/modules/mainNav/announcementTopBanner';
 
 import styles from './styles.css';
 
 export const links = () => {
   return [
-    { rel: 'stylesheet', href: styles },
+    {rel: 'stylesheet', href: styles},
     ...desktopNavItemStyles(),
     ...headerIconsStyles(),
     ...headerLogoStyles(),
@@ -29,56 +49,58 @@ export const links = () => {
   ];
 };
 
-const MainNav = ({ desktopHeaderNav, mobileOverlayNav, mobileNavbar, announcementHeader, announcementMessages, cartConfig, searchConfig, promoContent, mobileNavMainButton, products, annoucementTopBannerContent }) => {
-
+const MainNav = ({
+  desktopHeaderNav,
+  mobileOverlayNav,
+  mobileNavbar,
+  announcementHeader,
+  announcementMessages,
+  cartConfig,
+  searchConfig,
+  promoContent,
+  mobileNavMainButton,
+  products,
+  annoucementTopBannerContent,
+}) => {
   const mainContainer = useRef(null);
-  const { enableCountdownTimerPromo, countdownTimerPromoESTDeadlineDate, countdownTimerPromoESTDeadlineHour, countdownTimerPromoCopy, countdownTimerPromoBannerColor } = cartConfig;
+  const {
+    enableCountdownTimerPromo,
+    countdownTimerPromoESTDeadlineDate,
+    countdownTimerPromoESTDeadlineHour,
+    countdownTimerPromoCopy,
+    countdownTimerPromoBannerColor,
+  } = cartConfig;
 
   const handleClick = (e) => {
     const dataEvent = createCustomEvent();
-    if (document.querySelector('[data-visible-state]').getAttribute('data-visible-state') === 'show') {
+    if (
+      document
+        .querySelector('[data-visible-state]')
+        .getAttribute('data-visible-state') === 'show'
+    ) {
       e.currentTarget.parentElement.classList.remove('menuOverlayOpened');
-      document.querySelector('html').classList.remove('bodyWrap');
-      document.querySelector('[data-visible-state]').setAttribute('data-visible-state', 'hide');
+      document.querySelector('body').classList.remove('bodyWrap');
+      document
+        .querySelector('[data-visible-state]')
+        .setAttribute('data-visible-state', 'hide');
       document.querySelector('[data-visible-state]').dispatchEvent(dataEvent);
       mainContainer.current.style.zIndex = '10000';
     } else {
       e.currentTarget.parentElement.classList.add('menuOverlayOpened');
-      document.querySelector('html').classList.add('bodyWrap');
-      document.querySelector('[data-visible-state]').setAttribute('data-visible-state', 'show');
+      document.querySelector('body').classList.add('bodyWrap');
+      document
+        .querySelector('[data-visible-state]')
+        .setAttribute('data-visible-state', 'show');
       document.querySelector('[data-visible-state]').dispatchEvent(dataEvent);
       mainContainer.current.style.zIndex = '2147483001';
     }
   };
 
   useEffect(() => {
-    // Initialize navPlaceholder height onLoad
     if (document) {
-      const promoWrapperCheck = document.querySelectorAll('#promoBannersWrap').length > 0;
-      const promoWrapperCheckInterval = setInterval(() => {
-        if (promoWrapperCheck === true) {
-          document.getElementById('navPlaceholder').style.height = document.querySelector('.mainNav').offsetHeight + 'px';
-          document.getElementById('mobileOverlayWrapper').style.marginTop = document.getElementById('promoBannersWrap').offsetHeight + 36 + 'px';
-          document.querySelector('.mainNavMegaMenu').style.top = document.querySelector('.mainNavWrap').offsetHeight + 'px';
-          document.querySelectorAll('.navDropDown')[0].style.top = document.querySelector('.mainNavWrap').offsetHeight + 'px';
-          document.querySelectorAll('.navDropDown')[1].style.top = document.querySelector('.mainNavWrap').offsetHeight + 'px';
-          clearInterval(promoWrapperCheckInterval);
-        }
-      }, 1000);
-
-      // Add classname "close-promotional-banner" to any "close" buttons on promo bars we're adding in
-      let banners = document.querySelectorAll('.close-promotional-banner');
-      for (let i = 0; i < banners.length; i++) {
-        banners[i].addEventListener('click', function () {
-          banners[i].parentElement.style.display = 'none';
-          updateElemsPositionOnBannerClose();
-        });
-      }
-
       const anchor = document.querySelectorAll('a');
-      anchor.forEach(el => el.target = '_self');
+      anchor.forEach((el) => (el.target = '_self'));
     }
-
   }, []);
 
   return (
@@ -89,18 +111,27 @@ const MainNav = ({ desktopHeaderNav, mobileOverlayNav, mobileNavbar, announcemen
           enable={enableCountdownTimerPromo}
           copy={countdownTimerPromoCopy}
           bgColor={countdownTimerPromoBannerColor}
-          deadline={{ date: countdownTimerPromoESTDeadlineDate, hour: countdownTimerPromoESTDeadlineHour }}
+          deadline={{
+            date: countdownTimerPromoESTDeadlineDate,
+            hour: countdownTimerPromoESTDeadlineHour,
+          }}
         />
         <AnnouncementTopBanner content={annoucementTopBannerContent} />
       </div>
       <div className={'mainNavWrap mainNav'}>
         <div className={'relativeNav'}>
-
-          <NavTopHeader announcementHeader={announcementHeader} announcementMessages={announcementMessages} />
+          <NavTopHeader
+            announcementHeader={announcementHeader}
+            announcementMessages={announcementMessages}
+          />
 
           <div className={'navItemsWrap'}>
             <div className={'menu'}>
-              <div className={'hamburgerWrapper'} id='nav_click_hamburger' onClick={handleClick}>
+              <div
+                className={'hamburgerWrapper'}
+                id="nav_click_hamburger"
+                onClick={handleClick}
+              >
                 <HamburguerIcon />
                 <div className={'closeBtn'}>+</div>
               </div>
@@ -109,40 +140,75 @@ const MainNav = ({ desktopHeaderNav, mobileOverlayNav, mobileNavbar, announcemen
             <div className={'headerWrapper'}>
               <HeaderLogo />
             </div>
-            <div
-              id='nav_click_desktop_t1'
-              className={'navItemsContainer'}>
+            <div id="nav_click_desktop_t1" className={'navItemsContainer'}>
               {desktopHeaderNav?.headerNavItems?.map((navItem, index) => (
                 <DesktopNavItem
                   key={index}
                   navItem={navItem}
-                  itemId={`${index}-${navItem.displayText.replace(' ', '-').toLowerCase()}`}
-                />))
-              }
+                  itemId={`${index}-${navItem.displayText
+                    .replace(' ', '-')
+                    .toLowerCase()}`}
+                />
+              ))}
             </div>
-            <HeaderIcons cartConfig={cartConfig} searchConfig={searchConfig} products={products} />
+            <HeaderIcons
+              cartConfig={cartConfig}
+              searchConfig={searchConfig}
+              products={products}
+            />
           </div>
         </div>
       </div>
 
       <MobileNavbar content={mobileNavbar} />
 
-      <MainNavMobileOverlay mobileOverlayItems={mobileOverlayNav?.headerNavItems} mobileNavMainButton={mobileNavMainButton} />
-
+      <MainNavMobileOverlay
+        mobileOverlayItems={mobileOverlayNav?.headerNavItems}
+        mobileNavMainButton={mobileNavMainButton}
+      />
     </section>
   );
 };
 
-export const updateElemsPositionOnBannerClose = () => {
-  document.getElementById('navPlaceholder').style.height = document.querySelector('.mainNav').offsetHeight + 'px';
-  document.getElementById('mobileOverlayWrapper').style.marginTop = document.getElementById('promoBannersWrap').offsetHeight + 36 + 'px';
-  let navDropDown = document.querySelectorAll('.navDropDown');
-  for (let j = 0; j < navDropDown.length; j++) {
-    navDropDown[j].style.top = document.querySelector('.mainNav').offsetHeight + 'px';
-  }
-  document.querySelector('.mainNavMegaMenu').style.top = document.querySelector('.mainNav').offsetHeight + 'px';
-};
-
-const HamburguerIcon = () => <svg width={21} height={16} viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1={0.75} y1={1.25} x2={15.75} y2={1.25} stroke="#4C4E56" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" /><line x1={0.75} y1={11.25} x2={15.75} y2={11.25} stroke="#4C4E56" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" /><line x1={0.75} y1={6.25} x2={15.75} y2={6.25} stroke="#4C4E56" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const HamburguerIcon = () => (
+  <svg
+    width={21}
+    height={16}
+    viewBox="0 0 17 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line
+      x1={0.75}
+      y1={1.25}
+      x2={15.75}
+      y2={1.25}
+      stroke="#4C4E56"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <line
+      x1={0.75}
+      y1={11.25}
+      x2={15.75}
+      y2={11.25}
+      stroke="#4C4E56"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <line
+      x1={0.75}
+      y1={6.25}
+      x2={15.75}
+      y2={6.25}
+      stroke="#4C4E56"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 export default MainNav;
--- app/modules/search/index.jsx
+++ app/modules/search/index.jsx
@@ -1,17 +1,17 @@
-import { useRef, useEffect, useState } from 'react';
-import { bindCustomEvent, createCustomEvent } from '~/utils/functions/eventFunctions';
-import { useCollection } from '~/hooks/useCollection';
+import {useRef, useEffect, useState} from 'react';
+import {
+  bindCustomEvent,
+  createCustomEvent,
+} from '~/utils/functions/eventFunctions';
+import {useCollection} from '~/hooks/useCollection';
 
 import styles from './styles.css';
 
 export const links = () => {
-  return [
-    { rel: 'stylesheet', href: styles },
-  ];
+  return [{rel: 'stylesheet', href: styles}];
 };
 
 const SearchProduct = (product) => {
-
   const thumbnail = product.images.nodes[0];
 
   return (
@@ -19,25 +19,18 @@ const SearchProduct = (product) => {
       <a href={'/products/' + product.handle} className={'searchProductLink'}>
         <img src={thumbnail?.url} alt={thumbnail.altText || ''} />
         <div className={'productInfo'}>
-          {product.alt_title !== undefined &&
-            <div className={'productAltSearch'}>
-              {product.alt_title}
-            </div>
-          }
-          <div className={'productName'}>
-            {product.name}
-          </div>
+          {product.alt_title !== undefined && (
+            <div className={'productAltSearch'}>{product.alt_title}</div>
+          )}
+          <div className={'productName'}>{product.name}</div>
         </div>
       </a>
     </div>
   );
-
 };
 
 const showCB = (handleClose) => {
-
   if (!document.querySelector('.searchOverlay')) {
-
     const searchOverlay = document.createElement('div');
     const searchNav = document.querySelector('.searchNav');
     const isMobile = /Mobi/.test(window.navigator.userAgent);
@@ -47,16 +40,14 @@ const showCB = (handleClose) => {
 
     if (!isMobile) {
       searchOverlay.style.top = searchNav.style.top = `${getBannersHeight()}px`;
-      document.querySelector('html').classList.add('bodyWrap');
+      document.querySelector('body').classList.add('bodyWrap');
     }
 
     document.body.append(searchOverlay);
-
   }
 };
 
 const hideCB = () => {
-
   const searchNav = document.querySelector('.searchNav');
   const isMobile = /Mobi/.test(window.navigator.userAgent);
 
@@ -64,60 +55,52 @@ const hideCB = () => {
   document.querySelector('.searchOverlay').remove();
 
   if (!isMobile) {
-    document.querySelector('html').classList.remove('bodyWrap');
+    document.querySelector('body').classList.remove('bodyWrap');
   }
-
 };
 
 const getBannersHeight = () => {
-
   if (typeof document !== 'undefined') {
+    const promoBannerWrapHeight =
+      document.querySelector('.promoBannersWrap')?.offsetHeight;
+    const topHeaderContainerHeight = document.querySelector(
+      '.topHeaderContainer',
+    )?.offsetHeight;
+    const totalHeight = promoBannerWrapHeight + topHeaderContainerHeight;
 
-    const promoBannerWrapHeight = document.querySelector('.promoBannersWrap')?.offsetHeight;
-    const topHeaderContainerHeight = document.querySelector('.topHeaderContainer')?.offsetHeight;
-    const totalHeight = (promoBannerWrapHeight + topHeaderContainerHeight);
-
-    return (isNaN(totalHeight)) ? 0 : totalHeight;
-
+    return isNaN(totalHeight) ? 0 : totalHeight;
   } else {
-
     return 0;
-
   }
-
 };
 
-const Search = ({ searchConfig }) => {
+const Search = ({searchConfig}) => {
   const config = searchConfig;
   const searchRef = useRef(null);
   const inputRef = useRef(null);
 
-  const { state, products } = useCollection('all');
+  const {state, products} = useCollection('all');
   const [searchProducts, setSearchProducts] = useState([]);
 
   useEffect(() => {
-
     if (state === 'loaded') {
-
       const filteredProds = config.searchProducts.map((prods) => {
-        return products.filter(product => product.handle === prods.productId)[0];
+        return products.filter(
+          (product) => product.handle === prods.productId,
+        )[0];
       });
 
       setSearchProducts(filteredProds);
-
     }
-
   }, [state]);
 
   useEffect(() => {
-
     bindCustomEvent(searchRef, 'data-search-state', {
       hidden: 'hiddenSearch',
       visible: 'visibleSearch',
       hideCB,
-      showCB: () => showCB(handleClose)
+      showCB: () => showCB(handleClose),
     });
-
   }, []);
 
   function handleClose() {
@@ -129,18 +112,34 @@ const Search = ({ searchConfig }) => {
   }
   const handleSearch = (event) => {
     if (event.keyCode === 13) {
-      window.location.href = '/search?query=' + encodeURIComponent(event.target.value);
+      window.location.href =
+        '/search?query=' + encodeURIComponent(event.target.value);
     }
   };
   const handleClick = () => {
-    window.location.href = '/search?query=' + encodeURIComponent(inputRef.current.value);
+    window.location.href =
+      '/search?query=' + encodeURIComponent(inputRef.current.value);
   };
   return (
-    <div className={'searchNav hiddenSearch'} ref={searchRef} data-search-state="hide">
-
+    <div
+      className={'searchNav hiddenSearch'}
+      ref={searchRef}
+      data-search-state="hide"
+    >
       <div className={'searchForm'}>
-        <svg className={'searchIcon'} width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handleClick}>
-          <path d="M1.6696 9.74384C3.72631 11.8005 6.95947 11.9686 9.21225 10.2076L13.7996 14.7941C14.0742 15.0686 14.5194 15.0686 14.794 14.7941C15.0687 14.5196 15.0687 14.0745 14.794 13.8L10.2067 9.21348C11.9514 6.97624 11.8002 3.72995 9.74342 1.67327C7.51104 -0.558393 3.90072 -0.557118 1.66962 1.67327C-0.55654 3.89743 -0.55654 7.51854 1.66962 9.74398L1.6696 9.74384ZM2.66405 2.66609C4.34662 0.984055 7.06775 0.984693 8.74905 2.66609C10.426 4.34302 10.426 7.07203 8.74905 8.74912C7.06648 10.4312 4.34535 10.4305 2.66405 8.74912C0.98658 7.07267 0.98658 4.34318 2.66405 2.66609Z" fill="#4C4E56" />
+        <svg
+          className={'searchIcon'}
+          width="15"
+          height="15"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={handleClick}
+        >
+          <path
+            d="M1.6696 9.74384C3.72631 11.8005 6.95947 11.9686 9.21225 10.2076L13.7996 14.7941C14.0742 15.0686 14.5194 15.0686 14.794 14.7941C15.0687 14.5196 15.0687 14.0745 14.794 13.8L10.2067 9.21348C11.9514 6.97624 11.8002 3.72995 9.74342 1.67327C7.51104 -0.558393 3.90072 -0.557118 1.66962 1.67327C-0.55654 3.89743 -0.55654 7.51854 1.66962 9.74398L1.6696 9.74384ZM2.66405 2.66609C4.34662 0.984055 7.06775 0.984693 8.74905 2.66609C10.426 4.34302 10.426 7.07203 8.74905 8.74912C7.06648 10.4312 4.34535 10.4305 2.66405 8.74912C0.98658 7.07267 0.98658 4.34318 2.66405 2.66609Z"
+            fill="#4C4E56"
+          />
         </svg>
         <input
           type="text"
@@ -155,32 +154,31 @@ const Search = ({ searchConfig }) => {
         </div>
       </div>
 
-      <div className={'searchHeaders'}>
-        Trending Searches
-      </div>
+      <div className={'searchHeaders'}>Trending Searches</div>
 
       <div className={'searchTrendingWrap'}>
         <div className={'searchTrending'}>
           {config?.searchTags?.map((tag) => {
-            return <div className={'searchTag'} key={tag}>
-              <a href={'/search?query=' + tag}>{tag}</a>
-            </div>;
+            return (
+              <div className={'searchTag'} key={tag}>
+                <a href={'/search?query=' + tag}>{tag}</a>
+              </div>
+            );
           })}
         </div>
       </div>
 
-      <div className={'searchHeaders'}>
-        Trending Products
-      </div>
+      <div className={'searchHeaders'}>Trending Products</div>
 
-      {(searchProducts.length > 0) && <>
-        <div className={'searchTrendingProducts'}>
-          {searchProducts.map((product, index) => {
-            return (<SearchProduct {...product} key={index} />);
-          })}
-        </div>
-      </>}
-
+      {searchProducts.length > 0 && (
+        <>
+          <div className={'searchTrendingProducts'}>
+            {searchProducts.map((product, index) => {
+              return <SearchProduct {...product} key={index} />;
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
