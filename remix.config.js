5e41c85b457ab68e77348afc5ec9b661558aa42a
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-10-31 19:43:13 -0300

    :bug: fix: Remove routes v2 warning from Oxygen server

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+const {createRoutesFromFolders} = require('@remix-run/v1-route-convention');
+
 module.exports = {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
@@ -18,5 +20,10 @@ module.exports = {
   serverMinify: process.env.NODE_ENV === 'production',
   future: {
     v2_errorBoundary: true,
+    v2_routeConvention: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
   },
 };
