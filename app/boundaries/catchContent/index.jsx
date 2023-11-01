5698fe26dfee0b4f731a128c749222ea0fa43a29
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-11-01 15:41:34 -0300

    :zap: perf: Make listrakRec call to Sanity synchronous

--- app/boundaries/catchContent/index.jsx
+++ app/boundaries/catchContent/index.jsx
@@ -1,29 +1,29 @@
 import Layouts from '~/layouts';
-import PageNotFound, { links as pageNotFoundStyles } from '~/boundaries/catchContent/pageNotFound';
-import ListrakRec, { links as listrakRecStyles } from '~/modules/listrakRec';
-import { useMatches } from '@remix-run/react';
-import { getCMSDoc } from '~/utils/functions/eventFunctions';
+import PageNotFound, {
+  links as pageNotFoundStyles,
+} from '~/boundaries/catchContent/pageNotFound';
+import ListrakRec, {links as listrakRecStyles} from '~/modules/listrakRec';
+import {Await, useMatches} from '@remix-run/react';
+import {getCMSDoc} from '~/utils/functions/eventFunctions';
+import {Suspense} from 'react';
 
 export const links = () => {
-  return [
-    ...pageNotFoundStyles(),
-    ...listrakRecStyles(),
-  ];
+  return [...pageNotFoundStyles(), ...listrakRecStyles()];
 };
 
-export default function CatchContent({ status }) {
-
+export default function CatchContent({status}) {
   const [root] = useMatches();
 
   return (
-
     <Layouts.MainNavFooter>
-      {
-        (status === 404) ? <PageNotFound /> : <p>Caught - Status: {status}</p>
-      }
-      <ListrakRec listrak={getCMSDoc(root.data.listrakRec, '404')} />
+      {status === 404 ? <PageNotFound /> : <p>Caught - Status: {status}</p>}
+      <Suspense>
+        <Await resolve={root.data.listrakRec}>
+          {(listrakRec) => (
+            <ListrakRec listrak={getCMSDoc(listrakRec, '404')} />
+          )}
+        </Await>
+      </Suspense>
     </Layouts.MainNavFooter>
-
   );
-
 }
