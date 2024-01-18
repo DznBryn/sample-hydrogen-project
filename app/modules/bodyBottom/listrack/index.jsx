cfc1579c5e4611d79b488ba0f55df767cbabe8f4
Wilson Freitas <wilsondefreitas92@gmail.com>
2024-01-17 22:28:35 -0300

    :sparkles: feat: Implement LT page browse

--- app/modules/bodyBottom/listrack/index.jsx
+++ app/modules/bodyBottom/listrack/index.jsx
@@ -1,47 +1,44 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
-const Listrack = ({ productList }) => {
+const Listrack = ({productList}) => {
+  const triggerListrak = () => {
+    window._ltk.Signup.New(
+      'Footer',
+      'email_signup_new',
+      window._ltk.Signup.TYPE.CLICK,
+      'FooterNewsletterSubmit',
+      'channel',
+    );
 
-  useEffect(() => {
-
-    const triggerListrak = () => {
-
-      window._ltk.Signup.New('Footer', 'email_signup_new', window._ltk.Signup.TYPE.CLICK, 'FooterNewsletterSubmit', 'channel');
-      
-      if(typeof window._ltk.OnsiteContent?.reload === 'function') window._ltk.OnsiteContent?.reload();
-
-      if (window.location.href.includes('/products/')) {
-
-        const handle = window.location.pathname.replace('/products/', '');
-
-        const product = productList.products.find((prod) => (prod.handle === handle));
-
-        if(product){
-
-          const splittedID = product.id.split('/');
-          const externalID = splittedID[splittedID.length-1];
-          
-          window._ltk.Activity.AddProductBrowse(externalID);
-          
-        }
-
-      }
-
-      window._ltk.Activity.AddPageBrowse(window.location.href);
-
-    };
-
-    if (typeof window._ltk === 'object') {
-
-      triggerListrak();
-
+    if (typeof window._ltk.OnsiteContent?.reload === 'function') {
+      window._ltk.OnsiteContent?.reload();
     }
 
+    if (window.location.href.includes('/products/')) {
+      const handle = window.location.pathname.replace('/products/', '');
 
-  }, []);
+      const product = productList.products.find(
+        (prod) => prod.handle === handle,
+      );
+
+      if (product) {
+        const splittedID = product.id.split('/');
+        const externalID = splittedID[splittedID.length - 1];
+
+        window._ltk.Activity.AddProductBrowse(externalID);
+      }
+    }
+
+    window._ltk.Activity.AddPageBrowse(window.location.href);
+  };
+
+  useEffect(() => {
+    if (typeof window._ltk === 'object') {
+      triggerListrak();
+    }
+  });
 
   return null;
-
 };
 
 export default Listrack;
