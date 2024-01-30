e3460e86444fe2b2b7f74ad411e880e5a4cbd38f
Wilson Freitas <wilsondefreitas92@gmail.com>
2024-01-30 17:53:54 -0300

    :bug: fix: Change storerocket locator implementation approach

--- app/modules/storeLocator/index.jsx
+++ app/modules/storeLocator/index.jsx
@@ -1,5 +1,4 @@
 import {useEffect} from 'react';
-import {appendScript} from '~/utils/functions/eventFunctions';
 
 import styles from './styles.css';
 
@@ -7,52 +6,34 @@ export const links = () => {
   return [{rel: 'stylesheet', href: styles}];
 };
 
-const StoreLocator = () => {
-  const storeLocatorConfig = {
-    id: 'mb4njwY4yV',
-  };
+let started = false;
 
+const storeLocatorConfig = {
+  selector: '.storerocket-store-locator',
+  account: 'mb4njwY4yV',
+};
+
+const StoreLocator = () => {
   useEffect(() => {
-    if (typeof window === 'object') {
-      if (!window.StoreRocket) {
-        appendScript(
-          'https://cdn.storerocket.io/js/widget-mb.js',
-          'storelocatorescript',
-          true,
-        );
-      }
+    if (window.StoreRocket && !started) {
+      started = true;
+      window.StoreRocket.init(storeLocatorConfig);
     }
-  });
+
+    return function cleanup() {
+      started = false;
+    };
+  }, [storeLocatorConfig]);
 
   return (
-    <>
-      <div id="store-locator" className={'fixedWidthPage minHeight container'}>
-        <h1 className={'h1'}>Store Locator</h1>
-        <h2 className={'h2'}>
-          Find TULA in one of our retail partners nearest you. You can also shop
-          TULA in Sephora Canada and Mecca Australia stores.
-        </h2>
-        <div
-          id="storerocket-widget"
-          style={{width: '100%'}}
-          data-storerocket-env="p"
-          data-storerocket-id={storeLocatorConfig.id}
-        >
-          <p style={{textAlign: 'center', fontSize: '13px', padding: '10px'}}>
-            Store locator is loading from StoreRocket{' '}
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://storerocket.io"
-              style={{fontSize: '13px'}}
-            >
-              Store Locator App
-            </a>
-            ...
-          </p>
-        </div>
-      </div>
-    </>
+    <div id="store-locator" className={'fixedWidthPage minHeight container'}>
+      <h1 className={'h1'}>Store Locator</h1>
+      <h2 className={'h2'}>
+        Find TULA in one of our retail partners nearest you. You can also shop
+        TULA in Sephora Canada and Mecca Australia stores.
+      </h2>
+      <div className="storerocket-store-locator"></div>
+    </div>
   );
 };
 export default StoreLocator;
