72f524a26bc0f83dbfad410a40e0cc91a4de3c63
Wilson Freitas <wilsondefreitas92@gmail.com>
2024-02-26 13:11:52 -0300

    :fire: Remove no index tag

--- app/modules/pageMeta/index.jsx
+++ app/modules/pageMeta/index.jsx
@@ -17,7 +17,6 @@ const PageMeta = () => {
   return (
     <>
       <meta charSet="utf-8" />
-      <meta name="robots" content="noindex" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
 
       {/* <!-- Twitter Card data --> */}
