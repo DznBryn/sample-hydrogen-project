039bce28cc22bf02d240dba4f3e05c8b29e0316f
Wilson Freitas <wilsondefreitas92@gmail.com>
2024-02-26 13:23:08 -0300

    :fire: Remove mainNavLayout from errorBoundary and catchBoundary

--- app/boundaries/catchContent/index.jsx
+++ app/boundaries/catchContent/index.jsx
@@ -1,29 +1,32 @@
-import Layouts from '~/layouts';
+// import Layouts from '~/layouts';
 import PageNotFound, {
   links as pageNotFoundStyles,
 } from '~/boundaries/catchContent/pageNotFound';
-import ListrakRec, {links as listrakRecStyles} from '~/modules/listrakRec';
-import {Await, useMatches} from '@remix-run/react';
-import {getCMSDoc} from '~/utils/functions/eventFunctions';
-import {Suspense} from 'react';
+// import ListrakRec, {links as listrakRecStyles} from '~/modules/listrakRec';
+// import {Await, useMatches} from '@remix-run/react';
+// import {getCMSDoc} from '~/utils/functions/eventFunctions';
+// import {Suspense} from 'react';
 
 export const links = () => {
-  return [...pageNotFoundStyles(), ...listrakRecStyles()];
+  return [
+    ...pageNotFoundStyles(),
+    // ...listrakRecStyles()
+  ];
 };
 
 export default function CatchContent({status}) {
-  const [root] = useMatches();
+  // const [root] = useMatches();
 
   return (
-    <Layouts.MainNavFooter>
-      {status === 404 ? <PageNotFound /> : <p>Caught - Status: {status}</p>}
-      <Suspense>
-        <Await resolve={root.data.listrakRec}>
-          {(listrakRec) => (
-            <ListrakRec listrak={getCMSDoc(listrakRec, '404')} />
-          )}
-        </Await>
-      </Suspense>
-    </Layouts.MainNavFooter>
+    // <Layouts.MainNavFooter>
+    <>{status === 404 ? <PageNotFound /> : <p>Caught - Status: {status}</p>}</>
+    // <Suspense>
+    //   <Await resolve={root.data.listrakRec}>
+    //     {(listrakRec) => (
+    //       <ListrakRec listrak={getCMSDoc(listrakRec, '404')} />
+    //     )}
+    //   </Await>
+    // </Suspense>
+    // </Layouts.MainNavFooter>
   );
 }
